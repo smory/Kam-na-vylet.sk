39b88922bd3033a5c8aec16fb1d428711aa4f7d2
Peter Smorada <smoradap@gmail.com>
2016-09-12 21:11:35 +0200

    oddeleny kod na nastavenie parametrov views a ich hodnot

--- kamnavylet/app/src/main/java/sk/smoradap/kamnavyletsk/DetailsActivity.java
+++ kamnavylet/app/src/main/java/sk/smoradap/kamnavyletsk/DetailsActivity.java
@@ -9,13 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -88,6 +83,14 @@ public class DetailsActivity extends AppCompatActivity implements KamNaVyletApi.
     @Bean
     KamNaVyletApi api;
 
+    @AfterViews
+    void configureViews(){
+        mImageRecyclerView.setNestedScrollingEnabled(false);
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+        collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(android.R.color.white));
+        setSupportActionBar(toolbar);
+
+    }
 
     @AfterViews
     public void testUrls(){
@@ -111,7 +114,6 @@ public class DetailsActivity extends AppCompatActivity implements KamNaVyletApi.
     void setUpViews(final AttractionDetails details){
         mImageAdapter = new ImageRecyclerAdapter(this, details.getImageUrls());
         mImageRecyclerView.setAdapter(mImageAdapter);
-        mImageRecyclerView.setNestedScrollingEnabled(false);
         mDetailsTextView.setText(Html.fromHtml(details.getDescription()));
         mTitle.setText(Html.fromHtml("<b>" + details.getName() + "</b>"));
         mTownTextView.setText(details.getTown());
@@ -121,11 +123,7 @@ public class DetailsActivity extends AppCompatActivity implements KamNaVyletApi.
                 .bitmapTransform(new CropCircleTransformation(this))
                 .into(mBaseIcon);
 
-        setSupportActionBar(toolbar);
-
         collapsingToolbarLayout.setTitle(Html.fromHtml("<b>" + details.getName() + "</b>"));
-        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
-        collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(android.R.color.white));
 
         for(Map.Entry<String,String> entry : details.getDetailsMap().entrySet()){
             detailsTable.addRow(entry.getKey(), entry.getValue());
@@ -135,8 +133,6 @@ public class DetailsActivity extends AppCompatActivity implements KamNaVyletApi.
 
     @Click(R.id.details_description_layout)
     void descriptionToggle(){
-
-        System.out.println("clicked....");
 
         ObjectAnimator animator;
         if(mDetailsTextView.getMaxLines() == 5){
