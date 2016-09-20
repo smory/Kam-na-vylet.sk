5f2127a4d8384c58391f5169f0eb39a95b6879fa
Peter Smorada <smoradap@gmail.com>
2016-09-20 20:37:35 +0200

    SearchView je otvoreny pri search aktivity s hladanym vyrazom

--- kamnavylet/app/src/main/java/sk/smoradap/kamnavyletsk/SearchActivity.java
+++ kamnavylet/app/src/main/java/sk/smoradap/kamnavyletsk/SearchActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -73,11 +76,40 @@ public class SearchActivity extends AppCompatActivity implements KamNaVyletApi.O
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
+            mProgressLayout.setVisibility(View.VISIBLE);
             onBackPressed();
             return true;
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_simple_search, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setQuery(mSearchTerm, false);
+        searchView.setIconified(false);
+        searchView.clearFocus();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                mSearchTerm = query;
+                performSearch();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return true;
     }
 
     @Override
