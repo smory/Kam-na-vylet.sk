fc0794e7f62a064f59cfb07f0aa75738d3ff62e7
Peter Smorada <smoradap@gmail.com>
2016-12-27 19:39:13 +0100

    Pridava leak canary

--- kamnavylet/app/src/main/java/sk/smoradap/kamnavyletsk/KamNaVyletApplication.java
+++ kamnavylet/app/src/main/java/sk/smoradap/kamnavyletsk/KamNaVyletApplication.java
@@ -3,11 +3,13 @@ package sk.smoradap.kamnavyletsk;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
+import com.squareup.leakcanary.LeakCanary;
 
 import io.fabric.sdk.android.Fabric;
 
 /**
- * Created by smora on 27.12.2016.
+ * Application na inicializaciu crashlytics a leak canary
+ * Created by psmorada on 27.12.2016.
  */
 public class KamNaVyletApplication extends Application {
 
@@ -15,6 +17,12 @@ public class KamNaVyletApplication extends Application {
     @Override
     public void onCreate(){
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
         Fabric.with(this, new Crashlytics());
+        LeakCanary.install(this);
     }
 }
